93ccc5989db1ade127556c07b866178aaf51681f
Adroaldo Ferreira <hygino@msn.com>
2022-01-30 14:00:37 -0300

    Handling Save in React

--- backend-system/src/main/java/com/dailycodebuffer/employee/controller/EmployeeController.java
+++ backend-system/src/main/java/com/dailycodebuffer/employee/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.employee.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dailycodebuffer.employee.model.Employee;
 import com.dailycodebuffer.employee.services.EmployeeService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
-	
-	//Utilisando contrutor ao invés de Autowired para injetar a dependência
+
+	// Utilisando contrutor ao invés de Autowired para injetar a dependência
 	private final EmployeeService employeeService;
 
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
-	
+
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeService.createEmployee(employee);
