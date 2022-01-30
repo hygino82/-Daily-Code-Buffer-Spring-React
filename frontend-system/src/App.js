4b7807938bea1821f8a4e835be304cd8ef0eb8de
Adroaldo Ferreira <hygino@msn.com>
2022-01-30 14:47:11 -0300

    Adding React Router 6 in Project

--- frontend-system/src/App.js
+++ frontend-system/src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <AddEmployee/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
