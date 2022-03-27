b1831de3941d884e590973938a4842be5bc20813
Adroaldo Ferreira <hygino@msn.com>
2022-03-27 08:43:35 -0300

    Adicionado CRUD para entidade Department

---
+++ src/main/java/com/dailycodebuffer/tutorial/controller/DepartmentController.java
@@ -0,0 +1,49 @@
+package com.dailycodebuffer.tutorial.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dailycodebuffer.tutorial.entity.Department;
+import com.dailycodebuffer.tutorial.service.DepartmentService;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@PostMapping
+	public Department saveDepartment(@RequestBody Department department) {
+		return departmentService.saveDepartment(department);
+	}
+
+	@GetMapping
+	public List<Department> fetchDepartmentList() {
+		return departmentService.fetchDepartmentList();
+	}
+
+	@GetMapping("/{id}")
+	public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+		return departmentService.fetchDepartmentById(departmentId);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteDepartmentById(@PathVariable("id") Long departmentId) {
+		departmentService.deleteDepartmentById(departmentId);
+	}
+
+	@PutMapping("/{id}")
+	public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
+		return departmentService.updateDepartment(departmentId, department);
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO department(department_name, department_address, department_code) VALUES ('Limpeza','Linha Borges','326a52');
+INSERT INTO department(department_name, department_address, department_code) VALUES ('Costura','Barra Verde','eg473');
+INSERT INTO department(department_name, department_address, department_code) VALUES ('Transporte','Bom Jesus','925a3');
